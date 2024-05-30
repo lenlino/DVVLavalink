deb9fbd09c4cbe9caacb48791934cbc413130925
lenlino <65199014+lenlino@users.noreply.github.com>
2024-05-31 08:10:30 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+import aiofiles
+from fastapi import FastAPI, UploadFile
+from httpcore import ReadTimeout
+
+app = FastAPI()
+counter = 0
+
+@app.post("/send_wav")
+async def send_wav(file: UploadFile):
+    global counter
+    counter += 1
+    if counter > 300:
+        counter = 0
+    filename = "temp" + str(counter) + ".wav"
+    dir = os.path.dirname(os.path.abspath(__file__)) + "/voice/" + filename
+    try:
+        async with aiofiles.open(dir,
+                                 mode='wb') as f:
+            await f.write(await file.read())
+        return dir
+    except ReadTimeout:
+        return "error"
